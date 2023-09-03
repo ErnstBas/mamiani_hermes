7339ba6aca313e2d17dd70beaef47be352926203
Bas Ernst <zx88707@ou.ac.uk>
2023-09-03 17:08:07 +0200

    Add first files

--- article/models.py
+++ article/models.py
@@ -6,8 +6,8 @@ class Article(models.Model):
     title = models.CharField(max_length=100)  
     text = models.TextField(max_lentgth=1000)
     author = models.ManyToManyField(Author.author)
-    date = models.DateField()
-    image =models.ImageField()
+    date = models.DateField(auto_now=True)
+    image =models.ImageField(upload_to ='images')
 
 
     
